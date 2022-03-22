460d553727991c3f4c5f39179775c8b5aca5e890
Max-691 <mcluzel@edenschool.fr>
2022-03-22 22:27:05 +0100

    Finished project

--- Nb_sort_letter_position/js/app.js
+++ Nb_sort_letter_position/js/app.js
@@ -1,5 +1,21 @@
-let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let tmp = 0;
-for (let i = 0; i < alphabet.length; i++) {
-    
+//let nb = prompt('Enter number between 1 and 26. It will be //return the letter\'s position');
+//let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", //"j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", //"v", "w", "x", "y", "z"];
+//
+//for (let i = 0; i < alphabet.length; i++) {
+//    console.log(alphabet[i]+ ' '+ i )
+//}
+//console.log(alphabet[nb - 1]);
+
+// Enter letter who return their position
+
+const nb2 = prompt('Enter letter (a-z) It will return the occurence number');
+let alphabet2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+for(let i = 0; i < alphabet2.length; i++){
+    if(nb2 == alphabet2[i]){
+        console.log('Letter input: ' +nb2 + '  Position: ' + (i+1))
+    }
+    else{
+        console.log('Retry')
+    }
 }
